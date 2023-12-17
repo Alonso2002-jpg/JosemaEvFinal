7eca99cbc05bfb03e4a1653a5ebdd689af430afb
Alonso <alonsojorge980313810@hotmail.com>
2023-12-17 01:55:29 +0100

    Archivos static y templates - properties y data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO cursos (nrc, nombre, descripcion) VALUES ('1234', 'Matemáticas', 'Curso introductorio de matemáticas');
+INSERT INTO cursos (nrc, nombre, descripcion) VALUES ('5678', 'Historia', 'Curso sobre historia universal');
+INSERT INTO cursos (nrc, nombre, descripcion) VALUES ('9012', 'Biología', 'Curso avanzado de biología molecular');
+
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Jose Manuel', 'Rivadeneyra', '12345678');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('María', 'González', '87654321');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Luis', 'Rodríguez', '56781234');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Ana', 'Fernández', '43218765');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Carlos', 'López', '34567812');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Laura', 'Martínez', '78123456');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Pedro', 'Sánchez', '65432187');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Marta', 'Díaz', '23456781');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Pablo', 'Ramírez', '87651234');
+INSERT INTO alumno (nombre, apellido, dni) VALUES ('Lucía', 'Gómez', '12348765');
